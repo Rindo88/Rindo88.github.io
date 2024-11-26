962c960d597f8932720404eefb0c7e80e0a27a64
ahmad <naufal.af0207@gmail.com>
2024-11-26 20:37:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var options = {
+    strings: ["Ahmad Fadhil", "Software Dev"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  };
+  
+  var typed = new Typed('#typed-text', options);
+});
+
+AOS.init();
